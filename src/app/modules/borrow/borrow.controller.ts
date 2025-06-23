f629466ec3493f4b9b0d4e11d7112adfd74aed8b
AH-Ratul <ahratul21@gmail.com>
2025-06-23 21:54:01 +0600

    wrap with tryCatch

--- src/app/modules/borrow/borrow.controller.ts
+++ src/app/modules/borrow/borrow.controller.ts
@@ -1,88 +1,70 @@
 import { Request, Response } from "express";
 import { Book } from "../book/book.model";
 import { Borrow } from "./borrow.model";
+import { tryCatch } from "../../utils/tryCatch";
 
-const borrowABook = async (req: Request, res: Response) => {
-  try {
-    const body = req.body;
+const borrowABook = tryCatch(async (req: Request, res: Response) => {
+  const body = req.body;
 
-    const book = await Book.findById(body.book);
+  const book = await Book.findById(body.book);
 
-    if (book && book.isAvailabe()) {
-      console.log("This Book is availabe for borrowing");
-    } else {
-      console.log("Book is not available");
-    }
-
-    if (book) {
-      book.copies -= body.quantity;
-      await book.save();
-    }
-
-    const borrow = await Borrow.create(body);
-
-    res.status(201).json({
-      success: true,
-      message: "Book Borrowed successfully",
-      data: borrow,
-    });
-  } catch (error: any) {
-    //console.log(error);
-
-    res.status(400).json({
-      status: false,
-      message: error.name,
-      error,
-    });
+  if (book && book.isAvailabe()) {
+    console.log("This Book is availabe for borrowing");
+  } else {
+    console.log("Book is not available");
   }
-};
 
-const getBorrowSummary = async (req: Request, res: Response) => {
-  try {
-    const result = await Borrow.aggregate([
-      {
-        $lookup: {
-          from: "books",
-          localField: "book",
-          foreignField: "_id",
-          as: "book",
-        },
-      },
-      { $unwind: "$book" },
-      {
-        $group: {
-          _id: "$book",
-          title: { $first: "$book.title" },
-          isbn: { $first: "$book.isbn" },
-          totalQuantity: { $sum: "$quantity" },
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          book: {
-            title: "$title",
-            isbn: "$isbn",
-          },
-          totalQuantity: 1,
-        },
-      },
-    ]);
-
-    res.status(200).json({
-      status: true,
-      message: "Borrowed books summary retrieved successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
-
-    res.status(400).json({
-      status: false,
-      error,
-    });
+  if (book) {
+    book.copies -= body.quantity;
+    await book.save();
   }
-};
+
+  const borrow = await Borrow.create(body);
+
+  res.status(201).json({
+    success: true,
+    message: "Book Borrowed successfully",
+    data: borrow,
+  });
+});
+
+const getBorrowSummary = tryCatch(async (req: Request, res: Response) => {
+  const result = await Borrow.aggregate([
+    {
+      $lookup: {
+        from: "books",
+        localField: "book",
+        foreignField: "_id",
+        as: "book",
+      },
+    },
+    { $unwind: "$book" },
+    {
+      $group: {
+        _id: "$book",
+        title: { $first: "$book.title" },
+        isbn: { $first: "$book.isbn" },
+        totalQuantity: { $sum: "$quantity" },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        book: {
+          title: "$title",
+          isbn: "$isbn",
+        },
+        totalQuantity: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: true,
+    message: "Borrowed books summary retrieved successfully",
+    data: result,
+  });
+});
 
 export const borrowController = {
   borrowABook,
