57a770d5384b22f7ce856b9c4f7f345de4c90496
AH-Ratul <ahratul21@gmail.com>
2025-06-23 20:49:04 +0600

    handle validation error

---
+++ src/app/errorHandlers/handleValidationError.ts
@@ -0,0 +1,8 @@
+import { Error } from "mongoose";
+import { AppError } from "./AppError";
+
+export const handleValidationError = (err: Error.ValidationError) => {
+  const errors = Object.values(err.errors).map((val) => val.message);
+  const message = `${errors.join(". ")}`;
+  return new AppError(400, message);
+};
