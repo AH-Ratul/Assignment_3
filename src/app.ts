684b5fc25cc6d72b6bc0b14d2f1d717d4cb087ae
AH-Ratul <ahratul21@gmail.com>
2025-06-23 19:18:14 +0600

    manage globalError & api not found

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Response } from "express";
 import { appRoutes } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
+import { notFound } from "./app/middlewares/notFound";
 
 const app: Application = express();
 
@@ -10,5 +12,9 @@ app.use("/api", appRoutes);
 app.get("/", (_, res: Response) => {
   res.send("My Library app running...");
 });
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
 
 export default app;
