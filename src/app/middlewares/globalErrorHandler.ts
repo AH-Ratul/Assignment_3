c582487e8bc45d20c5d4eb90efab2f0da0da65a5
AH-Ratul <ahratul21@gmail.com>
2025-06-23 21:04:09 +0600

    handle global error & api not found

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,42 @@
+import { ErrorRequestHandler } from "express";
+import config from "../config";
+import { AppError } from "../errorHandlers/AppError";
+import { handleDuplicateField } from "../errorHandlers/handelDuplicateError";
+import { handleCastError } from "../errorHandlers/handleCastError";
+import { handleValidationError } from "../errorHandlers/handleValidationError";
+
+export const globalErrorHandler: ErrorRequestHandler = (
+  err: any,
+  req,
+  res,
+  next
+) => {
+  let statusCode = err.statusCode || 500;
+  let message = err?.message || "Something Went Wrong!!";
+
+  console.log("errrr", err)
+
+  if (err instanceof AppError) {
+    (statusCode = err?.statusCode), (message = err?.message);
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateField(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+  } else if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+  } else if (err.name === "ValidationError") {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+    err,
+    stack: config.NODE_ENV === "development" ? err.stack : null,
+  });
+};
